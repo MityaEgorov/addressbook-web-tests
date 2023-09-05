292d1afe364c066d69e5c8d9f9d7f6a33ed8c197
MityaEgorov <d.egorov@propellerads.net>
2023-09-05 15:29:15 +0200

    add

--- src/test/java/ru/auto/adressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/auto/adressbook/tests/GroupModificationTests.java
@@ -4,21 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.auto.adressbook.modal.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("Test", null, null));
-
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test", "Test2", "Test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returntoGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before );
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() );
   }
 }
